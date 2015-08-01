92a91d11e5fd76683a556892a3f306cade7f3299
yasinkuyu <yasinkuyu@gmail.com>
2015-08-01 21:17:55 +0300

    Fix redirect and message method

--- src/Message/Response.php
+++ src/Message/Response.php
@@ -1,21 +1,22 @@
-<?php namespace Omnipay\NestPay\Message;
+<?php
 
-use SimpleXMLElement;
+namespace Omnipay\NestPay\Message;
+
 use Omnipay\Common\Message\AbstractResponse;
 use Omnipay\Common\Message\RequestInterface;
 use Omnipay\Common\Message\RedirectResponseInterface;
 use Omnipay\Common\Exception\InvalidResponseException;
+use SimpleXMLElement;
 
 /**
  * NestPay Response
- * 
+ *
  * (c) Yasin Kuyu
  * 2015, insya.com
  * http://www.github.com/yasinkuyu/omnipay-nestpay
  */
-class Response extends AbstractResponse implements RedirectResponseInterface
-{
-   
+class Response extends AbstractResponse implements RedirectResponseInterface {
+
     /**
      * Constructor
      *
@@ -23,24 +24,22 @@ class Response extends AbstractResponse implements RedirectResponseInterface
      * @param  string                   $data
      * @throws InvalidResponseException
      */
-    public function __construct(RequestInterface $request, $data)
-    {
+    public function __construct(RequestInterface $request, $data) {
         $this->request = $request;
         try {
-            $this->data = new \SimpleXMLElement($data);
-        } catch (\Exception $e) {
+            $this->data = new SimpleXMLElement($data);
+        } catch (\Exception $ex) {
             throw new InvalidResponseException();
         }
     }
 
-     /**
+    /**
      * Whether or not response is successful
      *
      * @return bool
      */
-    public function isSuccessful()
-    {
-        return (string) $this->data->CC5Response->ProcReturnCode === '00';
+    public function isSuccessful() {
+        return (string) $this->data->CC5Response->ProcReturnCode === '00' || $this->data->CC5Response->Response === 'Approved';
     }
 
     /**
@@ -48,9 +47,8 @@ class Response extends AbstractResponse implements RedirectResponseInterface
      *
      * @return bool
      */
-    public function isRedirect()
-    {
-        return 3 === (int) $this->data->ErrMsg;
+    public function isRedirect() {
+        return false; //todo
     }
 
     /**
@@ -58,8 +56,7 @@ class Response extends AbstractResponse implements RedirectResponseInterface
      *
      * @return string
      */
-    public function getTransactionReference()
-    {
+    public function getTransactionReference() {
         return (string) $this->data->transid;
     }
 
@@ -68,10 +65,9 @@ class Response extends AbstractResponse implements RedirectResponseInterface
      *
      * @return string
      */
-    public function getMessage()
-    {
+    public function getMessage() {
         if ($this->isSuccessful()) {
-            return $this->data->response + " / "  + $this->data->host_msg;
+            return $this->data->host_msg;
         }
         return $this->data->CC5Response->error_msg;
     }
@@ -81,15 +77,28 @@ class Response extends AbstractResponse implements RedirectResponseInterface
      *
      * @return string
      */
-    public function getRedirectUrl()
-    {
+    public function getRedirectUrl() {
         if ($this->isRedirect()) {
             $data = array(
                 'TransId' => $this->data->TransId
             );
-            return $this->getRequest()->getEndpoint().'/test/index?'.http_build_query($data);
+            return $this->getRequest()->getEndpoint() . '/test/index?' . http_build_query($data);
         }
     }
-     
-    
+
+    public function getError() {
+        if ($this->isSuccessful()) {
+            return [];
+        }
+        return $this->data->CC5Response->error_msg;
+    }
+
+    public function getRedirectMethod() {
+        return 'POST';
+    }
+
+    public function getRedirectData() {
+        return null;
+    }
+
 }
