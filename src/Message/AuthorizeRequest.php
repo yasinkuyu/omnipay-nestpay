20a336729e68c8a06fa1fc4ecc6988a934dadda7
yasinkuyu <yasinkuyu@gmail.com>
2015-08-02 23:56:35 +0300

    Added AuthorizeRequest

---
+++ src/Message/AuthorizeRequest.php
@@ -0,0 +1,19 @@
+<?php namespace Omnipay\NestPay\Message;
+
+use Omnipay\Common\Message\AbstractRequest;
+
+/**
+ * NestPay Authorize Request
+ * 
+ * (c) Yasin Kuyu
+ * 2015, insya.com
+ * http://www.github.com/yasinkuyu/omnipay-nestpay
+ */
+class AuthorizeRequest extends AbstractRequest
+{
+
+    public function getData() {
+      
+    }
+ 
+}
